9b083375ed15cb4e589ed6d68245eec9312441a4
Josh <jbdelutis@gmail.com>
2020-06-30 07:55:55 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(jbdman, ender 3/btt skr 1_3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(jbdman, ender 3/btt skr 1_3/tmc2208/herome gen5/bltouch)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
